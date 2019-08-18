b22710002fa54c4ca3fa32800f807d76e788eb9b
OSSAMASHOKRY <OSSAMASHOKRY@DESKTOP-ISQQPVB.cicsv.eg.ibm.com>
2019-08-18 15:48:25 +0200

    in code test

--- IBMContractNotifier/src/com/ibm/contract/gui/UserInterface.java
+++ IBMContractNotifier/src/com/ibm/contract/gui/UserInterface.java
@@ -74,6 +74,7 @@ public class UserInterface {
 	private FileNameExtensionFilter txtFilter = new FileNameExtensionFilter("Text files", "txt");
 
 	public static void main(String[] args) {
+		System.out.println("test2");
 		EventQueue.invokeLater(new Runnable() {
 			public void run() {
 				try {
